69fd32d0818bf710b3782e2bbc975afc46989791
Antônio Sérgio G. Ferreira <antoniosergio@mail.com>
2015-09-16 22:36:33 -0300

    Cria o arquivo echome.sh e o preenche com o cabeçalho.

---
+++ echome.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#  echome.sh
+#
+#  Copyright 2015 Antônio Sérgio Garcia Ferreira <serginho@serginho-desktop>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#  2015/09/16 22:31:32
+
+#todo:
+#opcao -c para imprimir cabeçalho
+#opcao -l para imprimir licença
+#opcao -n para imprimir nome e email
+#opcao -r para imprimir readme
+#
+
